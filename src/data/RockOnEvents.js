349a59f7ae3ff85b71064061be583ec0881e4771
Ritik Saxena <ritiksaxena124@gmail.com>
2022-03-21 22:03:28 +0530

    Rockon Events Image added

--- src/data/RockOnEvents.js
+++ src/data/RockOnEvents.js
@@ -2,7 +2,7 @@ const events = [
     {
         name: "Voice of Invertia",
         tagline: "Let Your Heart Sing",
-        imglink:"img/Events/Rockon/voi.png",
+        imglink:"img/Events/Rockon/voi.jpg",
         venue: "Auditorium",
         date: "26th March, 2022",
         Time: "02:00 PM to 05:00 PM",
@@ -11,7 +11,7 @@ const events = [
       {
         name: "Streez",
         tagline: "The Street Dance",
-        imglink:"img/Events/Rockon/Streez.png",
+        imglink:"img/Events/Rockon/Streez.jpg",
         venue: "Three Way Junction",
         date: "25th March, 2022",
         Time: "03:00 PM to 05:00 PM",
@@ -20,7 +20,7 @@ const events = [
       {
         name: "Danspiration Reloaded",
         tagline: "",
-        imglink:"img/Events/Rockon/danspiration.png",
+        imglink:"img/Events/Rockon/danspiration.jpg",
         venue: "Auditorium",
         date: "26th March, 2022",
         Time: "10:00 AM to 01:00 PM",
